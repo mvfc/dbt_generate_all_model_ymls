718f514b41e71eb0ca4cd98ce0903540d5f1bda4
Matheus Vitório <mvfc@users.noreply.github.com>
2022-09-16 12:00:14 +0200

    Add v1 of generate_model_ymls.py

---
+++ generate_model_ymls.py
@@ -0,0 +1,54 @@
+import os
+import subprocess
+
+models_dir = os.getcwd()+'\models'
+
+dir_list = os.listdir(models_dir)
+
+def get_files_in_dir(dir):
+    return os.listdir(dir)
+
+def list_models_in_dir(dir):
+    model_list = []
+    for file in os.listdir(dir):
+        if '.' not in file:
+            inside_dir = list_models_in_dir(dir+f'\{file}')
+            for inside_file in inside_dir:
+                if '.sql' in inside_file:
+                    model_list.append(inside_file)
+                else:
+                    pass
+        elif '.sql' in file:
+            model_list.append(file)
+        else:
+            pass
+    return model_list
+
+def clean_model_list(model_list):
+    clean_list = []
+    for model in model_list:
+        clean_list.append(model.split('.')[0])
+    return clean_list
+
+def generate_node_yml(model_name):
+    try:
+        model_yml = subprocess.run("""dbt run-operation generate_model_yaml --args \"{\'model_name\':\'"""+model_name+"""\'}\" """, stdout=subprocess.PIPE).stdout.decode('utf-8')
+    except ValueError:
+        print(f'Error while running dbt command for model {model_name}')
+    return model_yml
+
+
+node_list = clean_model_list(list_models_in_dir(models_dir))
+for node in node_list:
+    yml = generate_node_yml(node).split('version: 2')[1]
+    yml = 'version: 2\n\n'+yml
+    try:
+        os.mkdir('generated_ymls')
+    except:
+        pass
+    try:
+        f = open(os.getcwd()+'\generated_ymls\\'+node+'.yml', "w")
+        f.write(yml)
+        f.close()
+    except:
+        print(f'Error on generating YML for node {node}')
